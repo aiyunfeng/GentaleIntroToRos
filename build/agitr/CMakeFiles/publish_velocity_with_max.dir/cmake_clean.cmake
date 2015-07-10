8b0406518422109663b86135bfe12fe1e8392113
He Sun <hesun@princeton.edu>
2015-07-10 00:14:22 -0400

    second part learning code, about lauch files and parameters

---
+++ build/agitr/CMakeFiles/publish_velocity_with_max.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/publish_velocity_with_max.dir/pubvel_with_max.cpp.o"
+  "/home/he/ros/devel/lib/agitr/publish_velocity_with_max.pdb"
+  "/home/he/ros/devel/lib/agitr/publish_velocity_with_max"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/publish_velocity_with_max.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,6 +7,12 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH="/home/he/ros/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/home/he/ros/devel/lib:/opt/ros/indigo/lib"
-export PKG_CONFIG_PATH="/home/he/ros/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/he/ros/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export CMAKE_PREFIX_PATH="/home/he/ros/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/he/ros/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/he/ros/devel/lib:/home/he/ros/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib"
+export PATH="/home/he/ros/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/he/ros/devel/lib/pkgconfig:/home/he/ros/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
 export PWD="/home/he/ros/build"
+export PYTHONPATH="/home/he/ros/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/he/ros/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/he/ros/src:$ROS_PACKAGE_PATH"
